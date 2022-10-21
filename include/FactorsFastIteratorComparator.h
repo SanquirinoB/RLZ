741e283987c5759264887cdb6b02131636b9da18
SanquirinoB <fernanda.sanchirico@ug.uchile.cl>
2022-10-21 11:28:39 -0300

    Optimized version

--- include/FactorsFastIteratorComparator.h
+++ include/FactorsFastIteratorComparator.h
@@ -137,63 +137,50 @@ public:
 			i_trigger = f_b.second;
 			isNextA = false;
 		}
-		// Para detectar cuando observamos la misma posicion dentro de la referencia
-		bool pointCoincidence = false;
-		unsigned long long delta = 0;
-
+		
 		for (unsigned long long i = 0; i <= i_trigger; i++)
 		{
 			// Si terminamos de leer para el largo mas corto o coincidimos con algun punto en la referencia
-			pointCoincidence = (f_a.first + i) == (f_b.first + i);
-			if (i == i_trigger || pointCoincidence)
+			if (i == i_trigger || (f_a.first == f_b.first))
 			{
-				delta = pointCoincidence? 0:i;
 				// Si ya no quedan factores por leer, se acabo
 				if (is_last) continue;
 				// Sino, tenemos que avanzar la lectura en base al factor mas corto
 				// Si debemos avanzar en a
 				if (isNextA)
 				{
-					// Si estamos avanzando por coincidencia de punto
-					if(pointCoincidence)
-					{
-						// Adelantamos la lectura equivalente al factor de a
-						f_b.first += f_a.second;
-						// Y actualizamos el largo del factor
-						f_b.second -= f_a.second;
-					}
+					// Adelantamos la lectura equivalente al factor de a
+					f_b.first += f_a.second;
+					// Y actualizamos el largo del factor
+					f_b.second -= f_a.second;
 					// Avanzamos el indice y leemos el factor sgte
 					a++;
 					f_a = factors[a];
 					// Y comparamos cual es el largo mas corto actual
-					if (f_a.second < f_b.second - delta)
+					if (f_a.second < f_b.second)
 					{
 						i_trigger = f_a.second;
 						isNextA = true;
 					}
 					else
 					{
-						i_trigger = f_b.second - delta;
+						i_trigger = f_b.second;
 						isNextA = false;
 					}
 				}
 				else
 				{
-					// Si estamos avanzando por coincidencia de punto
-					if(pointCoincidence)
-					{
-						// Adelantamos la lectura equivalente al factor de b
-						f_a.first += f_b.second;
-						// Y actualizamos el largo del factor
-						f_a.second -= f_b.second;
-					}
+					// Adelantamos la lectura equivalente al factor de b
+					f_a.first += f_b.second;
+					// Y actualizamos el largo del factor
+					f_a.second -= f_b.second;
 					// Avanzamos el indice y leemos el factor sgte
 					b++;
 					f_b = factors[b];
 					// Y comparamos cual es el largo mas corto actual
-					if (f_a.second - delta < f_b.second)
+					if (f_a.second < f_b.second)
 					{
-						i_trigger = f_a.second - delta;
+						i_trigger = f_a.second;
 						isNextA = true;
 					}
 					else
@@ -208,8 +195,6 @@ public:
 				// Si alguno de los factores es terminal, notificamos el estado de final
 				if (a == n_factors || b == n_factors) is_last = true;
 				// Si resulta que estamos leyendo desde el mismo punto en LZ(S|R), son necesariamente iguales a partir de ahora
-				// Asi que nos quedamos con el resultado de (_a < _b)
-				if (a == b) break;
 			}
 			// Si queda por leer
 			if( ref_text[ f_a.first + i ] < ref_text[ f_b.first + i ] ){
